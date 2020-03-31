6266310b4e7f3e39c6886d7dc256138c4a1f5726
Erik Olsen <erik.s.olsen@gmail.com>
2020-03-31 18:22:58 -0500

    update Initial Pairs

--- src/InitialPairs.js
+++ src/InitialPairs.js
@@ -7,14 +7,14 @@ const InitialPairs = [
       {
         "created_at": "2020-03-26T01:54:49.241579", 
         "id": 1, 
-        "role": "indigo", 
+        "role": "HOME", 
         "username": "EO", 
         "uuid": "585f3483-4615-4c4b-b3b0-9b66962d2386"
       }, 
       {
         "created_at": "2020-03-26T01:54:49.261307", 
         "id": 12, 
-        "role": "blue", 
+        "role": "VISITOR", 
         "username": "CD", 
         "uuid": "c4802b7e-2419-49c5-abce-451e37718a43"
       }
@@ -29,14 +29,14 @@ const InitialPairs = [
       {
         "created_at": "2020-03-26T01:54:49.245380", 
         "id": 2, 
-        "role": "indigo", 
+        "role": "HOME", 
         "username": "NH", 
         "uuid": "ef21f2a2-671c-4683-87f6-93042dc4da97"
       }, 
       {
         "created_at": "2020-03-26T01:54:49.262685", 
         "id": 13, 
-        "role": "blue", 
+        "role": "VISITOR", 
         "username": "TP", 
         "uuid": "c39b0030-62ce-44f2-a660-d333496d9f47"
       }
@@ -51,14 +51,14 @@ const InitialPairs = [
       {
         "created_at": "2020-03-26T01:54:49.247317", 
         "id": 3, 
-        "role": "indigo", 
+        "role": "HOME", 
         "username": "BD", 
         "uuid": "ea83f549-b0dd-4b5e-8340-95cb0a7e50de"
       }, 
       {
         "created_at": "2020-03-26T01:54:49.264130", 
         "id": 14, 
-        "role": "blue", 
+        "role": "VISITOR", 
         "username": "MR", 
         "uuid": "d4c6cc01-5ce2-4d6a-8674-428a21e84e51"
       }
@@ -73,14 +73,14 @@ const InitialPairs = [
       {
         "created_at": "2020-03-26T01:54:49.248786", 
         "id": 4, 
-        "role": "indigo", 
+        "role": "HOME", 
         "username": "JH", 
         "uuid": "3a4742c0-8153-4d51-aaaf-b92bdfcbb1c8"
       }, 
       {
         "created_at": "2020-03-26T01:54:49.265665", 
         "id": 15, 
-        "role": "blue", 
+        "role": "VISITOR", 
         "username": "RP", 
         "uuid": "2c2f581a-e146-43ce-be96-82e1d2ac7c18"
       }
@@ -95,14 +95,14 @@ const InitialPairs = [
       {
         "created_at": "2020-03-26T01:54:49.250415", 
         "id": 5, 
-        "role": "indigo", 
+        "role": "HOME", 
         "username": "MS", 
         "uuid": "e4056271-90be-45fa-9940-5945af4e5b55"
       }, 
       {
         "created_at": "2020-03-26T01:54:49.267069", 
         "id": 16, 
-        "role": "blue", 
+        "role": "VISITOR", 
         "username": "RJ", 
         "uuid": "8ec16ded-6316-433e-b655-d094ce1f692e"
       }
@@ -117,14 +117,14 @@ const InitialPairs = [
       {
         "created_at": "2020-03-26T01:54:49.251895", 
         "id": 6, 
-        "role": "indigo", 
+        "role": "HOME", 
         "username": "ES", 
         "uuid": "10175032-93ca-4fb8-9578-4aaf55d6b049"
       }, 
       {
         "created_at": "2020-03-26T01:54:49.268469", 
         "id": 17, 
-        "role": "blue", 
+        "role": "VISITOR", 
         "username": "JL", 
         "uuid": "7de780c4-e3f9-4219-948d-a10c781c95df"
       }
@@ -139,14 +139,14 @@ const InitialPairs = [
       {
         "created_at": "2020-03-26T01:54:49.253477", 
         "id": 7, 
-        "role": "indigo", 
+        "role": "HOME", 
         "username": "KD", 
         "uuid": "0d7ce313-ab2b-4c46-a085-44a04e586523"
       }, 
       {
         "created_at": "2020-03-26T01:54:49.269987", 
         "id": 18, 
-        "role": "blue", 
+        "role": "VISITOR", 
         "username": "CP", 
         "uuid": "222d7752-2391-438d-bcc3-d9df32a2e8a0"
       }
