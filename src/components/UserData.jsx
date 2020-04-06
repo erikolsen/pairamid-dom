e7b1b9f05d0e25395180fbb3a3e24617b8c35aa5
Erik Olsen <erik.s.olsen@gmail.com>
2020-04-06 09:13:46 -0500

    Remove borders on users

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -5,7 +5,7 @@ const colorMapper = {
 }
 const UserData = ({user}) => {
     return (
-        <div className={`w-12 h-12 m-2 border rounded-full bg-${colorMapper[user.role]}-400 flex items-center justify-center`}>
+        <div className={`w-12 h-12 m-2 rounded-full bg-${colorMapper[user.role]}-400 flex items-center justify-center`}>
             <p className="text-white font-bold text-xs">{user.username.toUpperCase()}</p>
         </div>
     )
