28123f2de3ef28c6793597916ed1b0a09ae0a251
Erik Olsen <erik.s.olsen@gmail.com>
2020-06-15 21:19:07 -0500

    Add collapseable header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react'
 import logo from '../../assets/pairamid-logo.png';
 import { Link, useRouteMatch } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserFriends, faBalanceScale, faHistory, faCog, faCalendarAlt} from '@fortawesome/free-solid-svg-icons'
+import { faUserFriends, faBalanceScale, faHistory, faCog, faCalendarAlt, faBars } from '@fortawesome/free-solid-svg-icons'
 
 const ListIconLink = ({path, icon, text}) => {
     return (
-        <li className='text-gray mx-1 lg:my-2 lg:mx-0'>
+        <li className='text-gray mx-1 my-2 lg:mx-0'>
             <Link className='' to={path}>
                 <FontAwesomeIcon icon={icon} />
                 <span className='ml-2'>{text}</span>
@@ -17,15 +17,23 @@ const ListIconLink = ({path, icon, text}) => {
 
 const Header = () => {
     const match = useRouteMatch()
+    const [collapsed, setCollapsed] = useState(true)
+    let classes = collapsed ? 'hidden' : 'block my-4'
     return (
         <div>
-            <header className="flex items-center justify-between lg:pt-12 lg:block p-4 border-gray-border border-b-2 lg:justify-center lg:h-screen lg:border-r-2 lg:border-b-0">
-                <Link to='/'>
-                    <div className='lg:my-8'>
-                        <img src={logo} alt='Paramid Logo' width="169" height="40" className="w-full max-w-logo lg:mt-8" />
-                    </div>
-                </Link>
-                <ul className='flex lg:block text-xs lg:text-base'>
+            <header className="p-4 border-gray-border border-b-2 lg:pt-12 lg:block lg:justify-center lg:h-screen lg:border-r-2 lg:border-b-0">
+                <div className='flex items-center justify-between '>
+                    <Link to='/'>
+                        <div className='lg:my-8'>
+                            <img src={logo} alt='Paramid Logo' width="169" height="40" className="w-full max-w-logo lg:mt-8" />
+                        </div>
+                    </Link>
+                    <button className='focus:outline-none lg:hidden' onClick={(e)=> setCollapsed(!collapsed)}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                </div>
+
+                <ul className={`lg:block lg:text-base ${classes}`}>
                     <ListIconLink path={`${match.url}`} icon={faUserFriends} text='Today' />
                     <ListIconLink path={`${match.url}/frequency`} icon={faBalanceScale} text='Frequency' />
                     <ListIconLink path={`${match.url}/history`} icon={faHistory} text='History' />
