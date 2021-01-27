9ca2113c161d6452342c1751205ff001e4638245
Erik Olsen <erik.s.olsen@gmail.com>
2021-01-26 20:25:29 -0600

    Show last active date for teams

--- src/components/Admin/TeamLink.js
+++ src/components/Admin/TeamLink.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { format } from 'date-fns'
+
 
 const TeamLink = ({team}) => {
+    const displayDate = format(new Date(team.lastActive), 'MM/dd/yyyy')
     return (
         <li>
             <Link className='' to={`/team/${team.uuid}`}>
-                <span>Name: {team.name}({team.members})</span>
+                <span>{team.name} - Last Active {displayDate}</span>
             </Link>
         </li>
     )
