4fdd1176b945186e9134d503f44cef1f7b16b704
Erik Olsen <erik.s.olsen@gmail.com>
2022-01-09 12:28:05 -0600

    Better positioning for terms

--- src/components/TeamLayout/Terms.js
+++ src/components/TeamLayout/Terms.js
@@ -12,7 +12,7 @@ const Terms = () => {
   return (
     showTerms &&
     !termsAccepted && (
-      <footer className="z-50 fixed bottom-0 col-span-8 flex justify-between m-2">
+      <footer className="z-50 fixed bottom-0 w-screen my-2 p-4">
         <div className="bg-blue-100 shadow-lg rounded-lg border border-black p-4">
           <div className="flex">
             <p>
